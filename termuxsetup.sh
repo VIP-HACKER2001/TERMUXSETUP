bcc35813821e9ffd2f808bfdf186d056818de72c
VISHNU PREMIL <82073583+VIP-HACKER2001@users.noreply.github.com>
2021-10-17 23:30:39 +0530

    Add files via upload

---
+++ termuxsetup.sh
@@ -0,0 +1,108 @@
+#!bin/bash
+sleep 2
+apt install figlet -y
+apt install mpv -y
+apt install sl
+apt install neofetch -y
+apt install espeak -y
+clear
+sleep 2
+espeak 'WELLCOME TO  THE WORLD OF HACKING'
+echo "            WELLCOME TO  THE WORLD OF HACKING "
+sleep 2
+echo "            ------------------VIP-HACKER------------------"
+sleep 2
+echo "     [                    ] 0% "
+espeak '0%'
+sleep 2
+espeak '25%'
+echo "     [=====               ] 25%"
+sleep 2
+espeak '50%'
+echo "     [==========          ] 50%"
+sleep 2   
+espeak '75%'
+echo "     [===============     ] 75%"
+sleep 2
+espeak '100%'
+echo "     [====================] 100%"
+sleep 2
+espeak 'LODING'
+echo "    LODING**********************"
+sleep 2
+espeak 'CHECKING INTERNET CONNETIONS'
+echo "       CHECKING INTERNET CONNETIONS"
+sleep 2
+espeak 'CONNECTED'
+echo "        CONNECTED"
+sleep 2
+espeak 'CHANGING IP FOR SECURITY REASON'
+echo  "      CHANGING IP FOR SECURITY REASON"
+sleep 2
+espeak 'IP CHANGED'
+echo  "       IP CHANGED"
+espeak 'programme IS  processing'
+sleep 2         
+clear
+cd
+cd ..
+cd usr
+cd etc
+rm bash.bashrc
+cd
+cd TERMUXSETUP
+cp  bash.bashrc /data/data/com.termux/files/usr/etc
+mv terminal.mp3 /$HOME
+cd
+cd ..
+cd usr
+cd etc
+rm motd
+cd
+cd TERMUXSETUP
+cp motd /data/data/com.termux/files/usr/etc
+clear
+sleep 2
+espeak 'DONE'
+echo "        *********DONE*********"
+sleep 2
+neofetch
+sleep 2
+espeak 'RESTART THE TERMUX'
+echo "                    RESTART THE TERMUX"
+sleep 2
+clear
+espeak 'CREATED BY VISHNU PREMIL'
+echo "                  CREATED BY VISHNU PREMIL"
+sleep 2
+clear
+sleep 1
+echo "    We all know that earth is  mother and also know that our mother is diying ."
+sleep 1
+echo "      what are we doing nothing just thinking about you and your family"
+sleep 1
+echo "    one think we forgeting is that  if earth is diying you and your family is also diying too."
+sleep 1
+echo "        there is more thing's you have too see in the earth ."
+sleep 1
+echo "         We all are very lucky to have a mother like earth."
+sleep 1
+echo "         The earth is a creation of life with beautiful soul."
+sleep 2
+clear 
+sleep 1
+echo "             -------------- SAVE EARTH ---------------"
+sleep 2
+espeak ' this programme will self delete know'
+cd
+cd
+bash
+cd
+rm -rf TERMUXSETUP
+cd
+cd
+exit
+#VIP-HACKER
+               
+
+
